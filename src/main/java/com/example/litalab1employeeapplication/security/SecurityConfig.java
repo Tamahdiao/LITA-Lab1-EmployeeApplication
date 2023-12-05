0685067a4728d5b32f575e50ed11253fbcf13725
Maimouna Tamah <maimouna-tamah.diao.external@worldline.com>
2023-12-05 14:47:39 +0000

    add swagger config

--- src/main/java/com/example/litalab1employeeapplication/security/SecurityConfig.java
+++ src/main/java/com/example/litalab1employeeapplication/security/SecurityConfig.java
@@ -3,12 +3,20 @@ package com.example.litalab1employeeapplication.security;
 import com.example.litalab1employeeapplication.service.EmployeeDetailsServiceImpl;
 import com.example.litalab1employeeapplication.service.UserDetailsServiceImpl;
 import com.example.litalab1employeeapplication.service.UserInfoService;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +27,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -47,11 +56,15 @@ public class SecurityConfig {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(configurer -> configurer.authenticationEntryPoint(enterPoint))
                 .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/swagger-ui/*").permitAll()
+                        .requestMatchers("/v3/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/employees/add").permitAll()
                         .requestMatchers("/contributions/add").permitAll()
-                        .requestMatchers("/contributions/update/*").permitAll()
-                        .requestMatchers("/contributions/employee/**").permitAll()
+                        .requestMatchers("/contributions/update/**").fullyAuthenticated()
+                        .requestMatchers("/contributions/update/**").hasRole("ADMIN")
+                        .requestMatchers("/contributions/employee/**").fullyAuthenticated()
+                        .requestMatchers("/contributions/employee/{username}").access(new WebExpressionAuthorizationManager("principal.username == #username"))
                         .requestMatchers("/users/**").fullyAuthenticated()
                         .anyRequest().denyAll()
                 )
@@ -74,6 +87,25 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My REST API")
+                        .description("Some custom description of API.")
+                        .version("1.0").contact(new Contact().name("Sallo Szrajbman")
+                                .email( "www.baeldung.com").url("salloszraj@gmail.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
